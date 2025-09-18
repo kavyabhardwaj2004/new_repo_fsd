d998e10573a0882a2ad5bd6a1da644e6143b847a
kavyabhardwaj <kbspace2004@gmail.com>
2025-09-18 13:13:27 +0530

    Add files via upload

---
+++ day_6/datatype.js
@@ -0,0 +1,9 @@
+let name = 'kavya'
+let age = 23;
+let boolvalue = true;
+console.log("name:",name);
+console.log("age:",age);
+console.log("boolvalue:",boolvalue);
+
+
+
---
+++ day_6/typeconversion.js
@@ -0,0 +1,6 @@
+let strToNum = "34" + 34;
+console.log("strToNum:",strToNum);
+console.log("Type of strToNum:",typeof strToNum);
+
+
+let a = "23" + 23 + (-67) + "67" +true-false
